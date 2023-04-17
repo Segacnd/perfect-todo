e611ee0b084d738cfa54affd4b35d23bd4739331
harmmselution <bugkfbdhdh@gmail.com>
2023-04-17 15:03:20 +0300

    flx styles

--- src/ui/buttons/card-button/card-button.tsx
+++ src/ui/buttons/card-button/card-button.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import classNames from 'classnames';
 import styles from './card-button.module.css';
 
 type ButtonProps = {
@@ -7,10 +6,5 @@ type ButtonProps = {
   type: 'redirect' | 'delete' | 'complete';
 };
 export const CardButton: FC<ButtonProps> = ({ text, type }) => {
-  const buttonClassNames = classNames(styles.button, {
-    [styles.redirect]: type === 'redirect',
-    [styles.delete]: type === 'delete',
-    [styles.complete]: type === 'complete',
-  });
-  return <div className={buttonClassNames}>{text}</div>;
+  return <div className={`${styles[type]} ${styles.button}`}>{text}</div>;
 };
