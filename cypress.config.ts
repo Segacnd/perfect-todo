8fa2963276db1c6817bf156aab0e6c9d86c3aa9c
Segacnd <segabelka@yandex.ru>
2023-05-03 15:00:44 +0300

    add running timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 /* eslint-disable-next-line */
 export default defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
     baseUrl: 'http://localhost:5173/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
