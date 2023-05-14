97701926145680adbc74da1869637a0cabdf7432
harmmselution <bugkfbdhdh@gmail.com>
2023-05-14 19:50:00 +0300

    fix ci

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -2,12 +2,11 @@ import { userSelector } from '../redux/selectors';
 import { useAppSelector } from '../redux/store';
 
 export const useAuth = () => {
-  const { email, id, token, login, photoUrl } = useAppSelector(userSelector);
+  const { email, id, login, photoUrl } = useAppSelector(userSelector);
 
   return {
     isAuth: !!email,
     email,
-    token,
     id,
     login,
     photoUrl,
