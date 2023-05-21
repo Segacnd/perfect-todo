518f45af2853bda0bc4d436439ade92991140e0a
harmmselution <bugkfbdhdh@gmail.com>
2023-05-22 01:06:35 +0300

    fix3

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 /* eslint-disable-next-line */
 export default defineConfig({
   e2e: {
     defaultCommandTimeout: 10000,
-    baseUrl: "http://localhost:5173/",
+    baseUrl: 'http://localhost:5173/',
   },
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
